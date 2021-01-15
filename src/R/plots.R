dc336004ff192eb8be177458c6984bb867483e07
ewarchul <ewarchul@gmail.com>
2021-01-15 18:21:08 +0100

    Add plot functions

---
+++ src/R/plots.R
@@ -0,0 +1,49 @@
+library(stringr)
+library(plot3D)
+
+plot_2d = function(dfx, x, y, z) {
+  xlabel = rlang::enquo(x)
+  ylabel = rlang::enquo(y)
+  dfx %>%
+    ggplot2::ggplot(
+      aes(x = {{ x }}, y = {{ y }}, col = {{ z}})
+    ) + 
+    ggplot2::geom_point() +
+    ggplot2::scale_color_gradient(
+      low="yellow",
+      high="red"
+    ) +
+    ggplot2::theme_classic() +
+    ggplot2::theme(
+      axis.title = ggplot2::element_text(size = 15, face = "bold"),
+      axis.text = ggplot2::element_text(size = 15, face = "bold"),
+      legend.text = ggplot2::element_text(size = 15, face = "bold"),
+      legend.title = ggplot2::element_text(size = 15, face = "bold")
+    ) +
+  xlab(glue::glue("Pressure {rlang::as_label(xlabel)}")) +
+  ylab(glue::glue("Pressure {rlang::as_label(ylabel)}")) +
+  xlim(0, 60) +
+  ylim(0, 60)
+}
+
+
+plot_3d = function(dfx, x, y, z) {
+  x_ = dfx %>% dplyr::pull({{x}})
+  y_ = dfx %>% dplyr::pull({{y}})
+  z_ = dfx %>% dplyr::pull({{z}})
+  plot3D::scatter3D(
+    x = x_,
+    y = y_,
+    z = z_,
+    theta = 50,
+    phi = 10,
+    colkey = F,
+    colvar = NULL,
+    xlab = "Pressure P1",
+    ylab = "Pressure P2",
+    zlab = "Depth",
+    ticktype = "detailed",
+    d = 2,
+    cex = 1.5
+  )
+}
